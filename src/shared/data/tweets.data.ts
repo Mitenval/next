296c8c53778670270e4bfaac5b11415579237a5d
Max Kosov <maxim.kosov@quantumsoft.ru>
2025-08-20 14:21:01 +0700

    Initial commit

---
+++ src/shared/data/tweets.data.ts
@@ -0,0 +1,23 @@
+import type { ITweet} from '../types/tweet.interface'
+export const TWEETS: ITweet[] = [
+  {
+    author: 'Mitenval',
+    text: 'Frontend is wonderfull'
+  },
+  {
+    author: 'csswitch',
+    text: 'Tailwind is not cheating. Writing consistent UI fast is just smart.'
+  },
+  {
+    author: 'jsx_queen',
+    text: '95% of front-end bugs: "I forgot to add key` in map()"'
+  },
+  {
+    author: 'layout_lord',
+    text: 'Stop using 12 nested divs. CSS Grid exists. Use it.'
+  },
+  {
+    author: 'vite_ninja',
+    text: 'Vite is so fast it makes me suspicious. Like... how?'
+  }
+]
